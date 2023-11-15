e56be007da3bf4ad073967973844c2010fb907e9
narayan-pradhan <80329203+narayan-pradhan@users.noreply.github.com>
2023-11-15 15:52:21 +0530

    fix

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,7 +3,7 @@ from hello_world.hello import say_hello
 
 class TestHelloWorld(unittest.TestCase):
   def test_say_hello(self):
-    result = say_hello("world")
+    result = say_hello("World")
     self.assertEqual(result, "Hello, World!")
 
 if __name__ == "__main__":
